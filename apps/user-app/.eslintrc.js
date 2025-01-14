01d68c82129744a26a8b7f6fb92cf171734ddb1d
deepharsh943 <138728579+deepharsh943@users.noreply.github.com>
2025-01-14 23:55:57 +0530

    migrated database

--- apps/user-app/.eslintrc.js
+++ apps/user-app/.eslintrc.js
@@ -1,7 +1,8 @@
+// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  ignorePatterns: ["apps/**", "packages/**"],
+  extends: ["@repo/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
