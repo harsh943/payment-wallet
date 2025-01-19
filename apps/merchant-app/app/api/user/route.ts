c17aa4145bc99e2a751c6e060bf8b9e12b96502d
deepharsh943 <138728579+deepharsh943@users.noreply.github.com>
2025-01-19 11:20:28 +0530

    force-dynamic

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import prisma from "@repo/db/client";
-
+export const dynamic = 'force-dynamic'
 export const GET = async () => {
     await prisma.user.create({
         data: {
